7138c765d426f51d41f1117182ce0f616bc057e9
Camilo Mejia <camilosw@gmail.com>
2013-08-19 14:55:56 -0500

    Updated to flash-messages 0.2.0 and Bootstrap 3 RC 2

--- flash-messages-demo.js
+++ flash-messages-demo.js
@@ -2,31 +2,31 @@ if (Meteor.isClient) {
   Template.main.events({
     'click a.show-message' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.sendInfo("I'm a message.");
+      FlashMessages.sendInfo("I'm a message.");
     },
     'click a.clear-messages' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.clear();
+      FlashMessages.clear();
     },
     'click a.show-alert' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.sendAlert("You could be in trouble.");
+      FlashMessages.sendAlert("You could be in trouble.");
     },
     'click a.show-error' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.sendError("Stop! your doing something wrong with your life.");
+      FlashMessages.sendError("Stop! your doing something wrong with your life.");
     },
     'click a.show-success' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.sendSuccess("Great! you're a successful user.");
+      FlashMessages.sendSuccess("Great! you're a successful user.");
     },
     'click a.show-info' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.sendInfo("It's ok. Nothing important happening.");
+      FlashMessages.sendInfo("It's ok. Nothing important happening.");
     },
     'click a.show-message-scroll' : function (e, tmpl) {
       e.preventDefault();
-      Meteor.Messages.sendAlert("I'm here!");
+      FlashMessages.sendAlert("I'm here!");
     }
   });
 }
